6ad19511c4f8dd3136b2995c8b7da1da7c00d681
alivinfer <942122009@qq.com>
2025-02-20 17:27:44 +0800

    [add]day03-Maven 1.创建 Maven 项目示例 2.Junit 单元测试使用示例

---
+++ day03-Maven/src/main/java/com/alivinfer/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.alivinfer;
+
+/**
+ * @author Fer
+ * @version 1.0
+ * @description Maven 项目示例
+ * @date 2025/2/20
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
